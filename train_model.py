c49eb694ba780af0509e754d62df8a6984bfb481
CJLSweeney <cjlsweeney@gmail.com>
2025-05-08 19:47:29 -0700

    Implement and Run ML Pipeline

--- train_model.py
+++ train_model.py
@@ -12,13 +12,13 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the cencus.csv data
+# Load census.csv data
 project_path = "/home/cjlsw/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path) # your code here
 
-# TODO: split the provided data to have a train dataset and a test dataset
+# Split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20, random_state=42)# Your code here
 
@@ -34,7 +34,7 @@ cat_features = [
     "native-country",
 ]
 
-# TODO: use the process_data function provided to process the data.
+# Use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
     train,    # your code here
     categorical_features=cat_features,# use the train dataset 
@@ -51,7 +51,7 @@ X_test, y_test, _, _ = process_data(
     lb=lb,
 )
 
-# TODO: use the train_model function to train the model on the training dataset
+# Use the train_model function to train the model on the training dataset
 model = train_model(X_train, y_train) # your code here
 
 # save the model and the encoder
@@ -67,14 +67,14 @@ model = load_model(
     model_path
 ) 
 
-# TODO: use the inference function to run the model inferences on the test dataset.
+# Use the inference function to run the model inferences on the test dataset.
 preds = inference(model, X_test) # your code here
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
 print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
-# TODO: compute the performance on model slices using the performance_on_categorical_slice function
+# Compute the performance on model slices using the performance_on_categorical_slice function
 # iterate through the categorical features
 for col in cat_features:
     # iterate through the unique values in one categorical feature
